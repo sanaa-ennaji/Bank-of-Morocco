582787d1113edad1a25989ac27acbf0bb25cf080
mouche <144794503+Mouche00@users.noreply.github.com>
2023-12-03 23:07:07 +0000

    establish structure

---
+++ admin/index.php
@@ -0,0 +1,5 @@
+<?php
+
+    require("../app/views/admin/index.php");
+
+?>
---
+++ app/views/admin/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!--  HEAD  -->
+<?php include("components/head.html") ?>
+<!--  HEAD  -->
+
+<body class="h-[100vh] w-full flex">
+
+    <!--  SIDEBAR  -->
+    <?php include("components/sidebar.html") ?>
+    <!--  SIDEBAR  -->
+
+    <main class="w-[85%]">
+
+        <!--  HEADER  -->
+            <?php include("components/header.html") ?>
+        <!--  HEADER  -->
+        
+        <div>
+            
+        </div>
+    </main>
+</body>
+<style src="../public/assets/javascript/main.js"></style>
+</html>
