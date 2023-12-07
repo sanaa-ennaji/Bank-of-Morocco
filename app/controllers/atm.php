ceaa1dd2244c8de33952ede32bebea874dca0fb0
Your Name <you@example.com>
2023-12-07 12:30:42 +0100

    add all controllers

---
+++ app/controllers/atm.php
@@ -0,0 +1,55 @@
+<?php
+
+    require_once("../models/random.php");
+    require_once("../models/atm.php");
+
+    $atm = new Atm();
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        $longitude = $_POST['longitude'];
+        $latitude = $_POST['latitude'];
+        $address = $_POST['address'];
+        $bank_id = $_POST['bank_id'];
+
+    // ---------  EDIT --------- //
+
+        if(isset($_POST['submit']) && $_POST['submit'] == 'Edit') {
+
+            $id = $_POST['id'];
+
+            $atm->edit($id, $longitude, $latitude, $address, $bank_id);
+
+    // ---------  ADD --------- //
+
+        } else if(isset($_POST['submit']) && $_POST['submit'] == 'Submit') {
+
+            $random = new Random();
+            
+            $id = $random->get();
+            $atm->add($id, $longitude, $latitude, $address, $bank_id);
+        }
+
+        // header("Location: ../views/admin/index.php");
+    }
+
+    // ---------  DELETE --------- //
+
+    if($_SERVER['REQUEST_METHOD'] == 'GET') {
+
+        if(isset($_GET['id'])) {
+
+            $id = $_GET['id'];
+            $atm->delete($id);
+
+            header("Location: ../views/admin/index.php");
+        }
+
+    }
+
+    // ---------  DISPLAY --------- //
+    
+    $atms = $atm->display();
+
+
+?>
